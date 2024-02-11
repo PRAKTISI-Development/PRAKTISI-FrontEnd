0c95ebb9e9c7cb010a8d4c324e65888e65e1bf38
novilfahlevy <novilfreon@gmail.com>
2024-02-11 17:18:24 +0800

    remove subject field from add end edit schedule

--- app/mata-kuliah/[id_mata_kuliah]/jadwal/tambah/page.tsx
+++ app/mata-kuliah/[id_mata_kuliah]/jadwal/tambah/page.tsx
@@ -26,21 +26,6 @@ export default function AddInformationPage() {
             <div className="p-6.5">
               <div className="mb-4.5">
                 <label className="mb-2.5 block text-black dark:text-white">
-                  Mata Kuliah <span className="text-meta-1">*</span>
-                </label>
-                <Select
-                  name="matkul"
-                  placeholder="Mata Kuliah"
-                  options={[{
-                    value: '1', label: 'Dasar-dasar Pemrograman',
-                  }, {
-                    value: '2', label: 'Algoritma dan Struktur Data'
-                  }]}
-                />
-              </div>
-
-              <div className="mb-4.5">
-                <label className="mb-2.5 block text-black dark:text-white">
                   Tanggal <span className="text-meta-1">*</span>
                 </label>
                 <input
